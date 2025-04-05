f2069f0e70abb154e9c3fb3fa4d63a2f0f8f2e7c
Andrew Walton <163466371+AndrewASUS@users.noreply.github.com>
2025-04-05 12:16:55 +1100

    Overflow y on 404 page

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
   return (
     <>
       <div
-        className="overflow-y-hidden min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
+        className="relative overflow-hidden min-h-screen bg-cover bg-center flex flex-col justify-center items-center text-white"
         style={{ backgroundImage: `url('/404.png')` }}
       >
         <header className="absolute top-0 left-0 p-4 bg-black w-full ">
